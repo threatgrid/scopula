37058f1b194e5fb5f74aadc455a5c7362f8be0fe
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2019-06-14 16:15:35 +0200

    updated project to ease release workflow

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :description "Handle a scope convention mechanism to enhance control access"
   :url "https://github.com/threatgrid/scopula"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url "https://www.eclipse.org/legal/epl-2.0/"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :lein-release {:deploy-via :clojars
-                 :scm :git}
+  :deploy-repositories [["releases"  {:url "https://clojars.org/repo" :creds :gpg}]
+                        ["snapshots" {:url "https://clojars.org/repo" :creds :gpg}]]
   :repl-options {:init-ns scopula.core})
