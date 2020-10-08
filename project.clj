53551f0f9a8c32154cbc8807beafc34fddd5ea1e
vemv <vemv@users.noreply.github.com>
2020-10-08 18:57:34 +0200

    0.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject scopula "0.1.5-SNAPSHOT"
-  :description "Handle a scope convention mechanism to enhance control access"
+(defproject scopula "0.2.0"
+  :description "Offers a scope convention mechanism to enhance control access."
   :url "https://github.com/threatgrid/scopula"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :deploy-repositories [["releases"  {:url "https://clojars.org/repo" :creds :gpg}]
                         ["snapshots" {:url "https://clojars.org/repo" :creds :gpg}]]
   :repl-options {:init-ns scopula.core})
