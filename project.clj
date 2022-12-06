39b541add12d8bbba940e20ede3885ab77760fb5
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2022-12-06 14:03:43 +0100

    updated version and deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject threatgrid/scopula "0.2.0"
+(defproject threatgrid/scopula "0.3.0-SNAPSHOT"
   :description "Offers a scope convention mechanism to enhance control access."
   :url "https://github.com/threatgrid/scopula"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :deploy-repositories [["releases"  {:url "https://clojars.org/repo" :creds :gpg}]
                         ["snapshots" {:url "https://clojars.org/repo" :creds :gpg}]]
   :repl-options {:init-ns scopula.core})
