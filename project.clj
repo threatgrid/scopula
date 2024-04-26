151ce0045e8a4694af10e77b43d9235f21567e2d
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2024-04-26 10:00:01 +0200

    Version 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/scopula "0.3.2-SNAPSHOT"
+(defproject threatgrid/scopula "0.3.2"
   :description "Offers a scope convention mechanism to enhance control access."
   :url "https://github.com/threatgrid/scopula"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
