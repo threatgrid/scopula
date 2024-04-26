27c09f26c7f04b60da034fef5020ac5ab3b643f8
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2024-04-26 09:30:09 +0200

    support cljs

--- project.clj
+++ project.clj
@@ -4,7 +4,15 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.132"]]
   :deploy-repositories [["releases"  {:url "https://clojars.org/repo" :creds :gpg}]
                         ["snapshots" {:url "https://clojars.org/repo" :creds :gpg}]]
-  :repl-options {:init-ns scopula.core})
+  :repl-options {:init-ns scopula.core }
+  :plugins [[lein-cljsbuild "1.1.8"]]
+  :cljsbuild {:source-paths ["src"]
+              :compiler {:optimizations :whitespace
+                         :pretty-print false
+                         } 
+              }
+  )
